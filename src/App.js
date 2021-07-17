161ca396172ab9fc0bdd23f9a3f9ecf937c09167
Somesh Kirthik <spsomesh2001@gmail.com>
2021-07-17 17:28:44 +0530

    route-fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { GlobalCon } from "./globalStyles";
 import { Header, Main } from "./components/index";
